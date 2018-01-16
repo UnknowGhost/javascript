54165ebf1eb6b5b68d650b937392e81287d505b6
Justin <justin-z@live.nl>
2018-01-16 11:02:51 +0100

    okay *ear rape*

---
+++ mijnjavascript.js
@@ -0,0 +1,9 @@
+var mijnauto = {
+    merk: "Ford",
+    type: "Mondeo",
+    aantalWielen: 4,
+    kleur: "Blauw"
+}
+
+console.log (mijnauto.kleur);
+documet.getElementById("grotekop").innerHTML = mijnauto.kleur
---
+++ vulmetjs.js
@@ -0,0 +1,4 @@
+document.getElementById("paginatitel").innerText = "Mijn geweldige pagina";
+document.getElementById("subkop").innerText = "Mijn geweldige supkop";
+document.getElementById("aap1").src = "pictures/aap1.jpg";
+document.getElementById("aap2").src = "pictures/aap2.jpg";
